523d3a5edc4d58f4a5de93426e6b60e8900bc008
Nexxxit <varsik.arsik@mail.ru>
2025-09-01 21:51:35 +0500

    feat: добавить периÐ воременной период

---
+++ src/modules/currentPeriod/ui/CurrentPeriod.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  currentPeriod: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <p class="arsenal-sc-bold text-2xl">{{currentPeriod}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

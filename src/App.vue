c566748977440d7077f4ff52c265e91941947578
Nexxxit <varsik.arsik@mail.ru>
2025-09-01 18:07:22 +0500

    feat: доÐбавить header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 
+import Header from "./modules/core/header/ui/Header.vue";
 </script>
 
 <template>
-
+  <div class="bg-gray-400/10 h-screen p-2 arsenal-sc-regular">
+    <Header/>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/modules/core/header/ui/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="flex items-center backdrop-blur-lg bg-white/40 border border-white/30 shadow-lg shadow-black/10 rounded-3xl p-5">
+    <p class="font-bold text-3xl">MyCustomDiem</p>
+
+  </header>
+</template>
+
+<style scoped>
+
+</style>

585c740715d0bf238d31a7d67561326e7f35dcdd
Nexxxit <varsik.arsik@mail.ru>
2025-09-03 00:57:02 +0500

    feat: добавить возвращение на сегодняшнюю дату

--- src/modules/core/header/ui/Header.vue
+++ src/modules/core/header/ui/Header.vue
@@ -3,6 +3,12 @@
 import Navigation from "../../../navigation/ui/Navigation.vue";
 import Button from "../../../ud-ui/button/ui/Button.vue";
 import Dropdown from "../../../ud-ui/dropdown/ui/Dropdown.vue";
+import {useChangeDate} from "../../../navigation/store/useChangeDate.ts";
+
+const changeDate = useChangeDate()
+
+const {resetToToday} = changeDate
+
 </script>
 
 <template>
@@ -10,7 +16,7 @@ import Dropdown from "../../../ud-ui/dropdown/ui/Dropdown.vue";
   shadow-lg shadow-black/10 rounded-3xl p-5">
     <div class="flex gap-6">
       <p class="font-bold text-3xl">MyCustomDiem</p>
-      <Button title="Сегодняшняя дата" on-click="" text="Сегодня" class="py-1 px-2 rounded-full text-lg" />
+      <Button @click="resetToToday" title="Сегодняшняя дата" text="Сегодня" class="py-1 px-2 rounded-full text-lg" />
     </div>
     <Navigation/>
     <Dropdown />

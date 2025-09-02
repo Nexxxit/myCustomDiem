0dd057b56268ff7131ca99ebb87248b1c26baa50
Nexxxit <varsik.arsik@mail.ru>
2025-09-03 00:54:07 +0500

    feat: добавить возможность переключать дату

--- src/modules/navigation/ui/Navigation.vue
+++ src/modules/navigation/ui/Navigation.vue
@@ -4,27 +4,46 @@ import Button from "../../ud-ui/button/ui/Button.vue";
 import ChevronLeft from "../../../assets/ChevronLeft.vue";
 import ChevronRight from "../../../assets/ChevronRight.vue";
 import DatePeriod from "../../datePeriod/ui/DatePeriod.vue";
+import {useChangeDate} from "../store/useChangeDate.ts";
+import {useSelectView} from "../../ud-ui/dropdown/store/useSelectView.ts";
+import {storeToRefs} from "pinia";
 
-const date = new Date().toLocaleDateString(undefined, {
-  year: "numeric",
-  month: "long",
-  day: "numeric"
-})
+const changeDateStore = useChangeDate()
+const {prevDay, prevWeek, nextWeek, nextDay} = changeDateStore
+const {formattedDate} = storeToRefs(changeDateStore)
+const selectView = useSelectView()
+
+function handlePrev() {
+  if(selectView.selectedView === 'День') {
+    prevDay()
+  } else if (selectView.selectedView === 'Неделя') {
+    prevWeek()
+  }
+}
+
+function handleNext() {
+  if(selectView.selectedView === 'День') {
+    nextDay()
+  } else if (selectView.selectedView === 'Неделя') {
+    nextWeek()
+  }
+}
+
 </script>
 
 <template>
   <div class="flex gap-3 items-center justify-center">
-    <Button on-click="" title="Предыдущая дата" class="p-2 rounded-full justify-center">
+    <Button @click="handlePrev" title="Предыдущая дата" class="p-2 rounded-full justify-center">
       <template #icon>
-        <ChevronLeft />
+        <ChevronLeft/>
       </template>
     </Button>
 
-    <DatePeriod :current-period="date" />
+    <DatePeriod :current-period="formattedDate"/>
 
-    <Button on-click="" title="Следующая дата" class="p-2 rounded-full justify-center">
+    <Button @click="handleNext" title="Следующая дата" class="p-2 rounded-full justify-center">
       <template #icon>
-        <ChevronRight />
+        <ChevronRight/>
       </template>
     </Button>
   </div>
